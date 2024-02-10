bcc48f3884c6373a1ac7324ec95c14491bd0cde9
Jerry Ding <13386637338@qq.com>
2024-02-10 16:28:39 -0500

    minor fixes

--- networks/stackhourglass.py
+++ networks/stackhourglass.py
@@ -16,27 +16,31 @@ class hourglass(nn.Module):
     def __init__(self, inplanes):
         super(hourglass, self).__init__()
 
-        self.conv1 = nn.Sequential(convbn_3d(inplanes, inplanes*2, kernel_size=3, stride=2, pad=1),
+        self.conv1 = nn.Sequential(convbn_3d(inplanes, inplanes * 2, kernel_size=3, stride=2, pad=1),
                                    nn.ReLU(inplace=True))
 
-        self.conv2 = convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=1, pad=1)
+        self.conv2 = convbn_3d(inplanes * 2, inplanes * 2, kernel_size=3, stride=1, pad=1)
 
-        self.conv3 = nn.Sequential(convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=2, pad=1),
+        self.conv3 = nn.Sequential(convbn_3d(inplanes * 2, inplanes * 2, kernel_size=3, stride=2, pad=1),
                                    nn.ReLU(inplace=True))
 
-        self.conv4 = nn.Sequential(convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=1, pad=1),
+        self.conv4 = nn.Sequential(convbn_3d(inplanes * 2, inplanes * 2, kernel_size=3, stride=1, pad=1),
                                    nn.ReLU(inplace=True))
 
-        self.conv5 = nn.Sequential(nn.ConvTranspose3d(inplanes*2, inplanes*2, kernel_size=3, padding=1, output_padding=1, stride=2,bias=False),
-                                   nn.BatchNorm3d(inplanes*2)) #+conv2
+        self.conv5 = nn.Sequential(
+            nn.ConvTranspose3d(inplanes * 2, inplanes * 2, kernel_size=3, padding=1, output_padding=1, stride=2,
+                               bias=False),
+            nn.BatchNorm3d(inplanes * 2))  # +conv2
 
-        self.conv6 = nn.Sequential(nn.ConvTranspose3d(inplanes*2, inplanes, kernel_size=3, padding=1, output_padding=1, stride=2,bias=False),
-                                   nn.BatchNorm3d(inplanes)) #+x
+        self.conv6 = nn.Sequential(
+            nn.ConvTranspose3d(inplanes * 2, inplanes, kernel_size=3, padding=1, output_padding=1, stride=2,
+                               bias=False),
+            nn.BatchNorm3d(inplanes))  # +x
 
-    def forward(self, x ,presqu, postsqu):
+    def forward(self, x, presqu, postsqu):
 
-        out  = self.conv1(x) #in:1/4 out:1/8
-        pre  = self.conv2(out) #in:1/8 out:1/8
+        out = self.conv1(x)  # in:1/4 out:1/8
+        pre = self.conv2(out)  # in:1/8 out:1/8
 
         if postsqu is not None:
             pre = F.relu(pre + postsqu, inplace=True)
@@ -45,17 +49,17 @@ class hourglass(nn.Module):
 
         # print('pre2', pre.size())
 
-        out  = self.conv3(pre) #in:1/8 out:1/16
-        out  = self.conv4(out) #in:1/16 out:1/16
+        out = self.conv3(pre)  # in:1/8 out:1/16
+        out = self.conv4(out)  # in:1/16 out:1/16
 
         # print('out', out.size())
 
         if presqu is not None:
-            post = F.relu(self.conv5(out)+presqu, inplace=True) #in:1/16 out:1/8
+            post = F.relu(self.conv5(out) + presqu, inplace=True)  # in:1/16 out:1/8
         else:
-            post = F.relu(self.conv5(out)+pre, inplace=True)
+            post = F.relu(self.conv5(out) + pre, inplace=True)
 
-        out  = self.conv6(post)  #in:1/8 out:1/4
+        out = self.conv6(post)  # in:1/8 out:1/4
 
         return out, pre, post
 
@@ -83,7 +87,6 @@ class hourglass_gwcnet(nn.Module):
         self.redir2 = convbn_3d(inplanes * 2, inplanes * 2, kernel_size=1, stride=1, pad=0)
 
     def forward(self, x):
-
         conv1 = self.conv1(x)
         conv2 = self.conv2(conv1)
 
@@ -122,15 +125,15 @@ class PSMNet(nn.Module):
 
         self.classif1 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
                                       nn.ReLU(inplace=True),
-                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1, bias=False))
 
         self.classif2 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
                                       nn.ReLU(inplace=True),
-                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1, bias=False))
 
         self.classif3 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
                                       nn.ReLU(inplace=True),
-                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1, bias=False))
         if refine == 'csr':
             self.refine_module = DeformableRefineF(feature_c=64, node_n=2, modulation=True, cost=True)
         else:
@@ -141,7 +144,7 @@ class PSMNet(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.Conv3d):
-                n = m.kernel_size[0] * m.kernel_size[1]*m.kernel_size[2] * m.out_channels
+                n = m.kernel_size[0] * m.kernel_size[1] * m.kernel_size[2] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
@@ -157,11 +160,11 @@ class PSMNet(nn.Module):
         refimg_fea = self.feature_extraction(left)
         targetimg_fea = self.feature_extraction(right)
 
-        #matching
-        cost = torch.zeros(refimg_fea.size()[0], refimg_fea.size()[1]*2, self.maxdisp//4,
+        # matching
+        cost = torch.zeros(refimg_fea.size()[0], refimg_fea.size()[1] * 2, self.maxdisp // 4,
                            refimg_fea.size()[2], refimg_fea.size()[3]).cuda()
 
-        for i in range(self.maxdisp//4):
+        for i in range(self.maxdisp // 4):
             if i > 0:
                 cost[:, :refimg_fea.size()[1], i, :, i:] = refimg_fea[:, :, :, i:]
                 cost[:, refimg_fea.size()[1]:, i, :, i:] = targetimg_fea[:, :, :, :-i]
@@ -200,7 +203,8 @@ class PSMNet(nn.Module):
             pred2 = DisparityRegression(self.maxdisp, win_size=win_s)(distribute2)
 
         cost3 = self.classif3(out3)
-        cost3 = F.interpolate(cost3, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear', align_corners=True)
+        cost3 = F.interpolate(cost3, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear',
+                              align_corners=True)
         cost3 = torch.squeeze(cost3, 1)
         distribute3 = F.softmax(cost3, dim=1)
         pred3 = DisparityRegression(self.maxdisp, win_size=win_s)(distribute3)
@@ -227,8 +231,8 @@ class PSMNet(nn.Module):
                         CEloss(gt_left, self.maxdisp, gt_distribute, distribute3)
 
                 if self.refine:
-                    loss1 += F.smooth_l1_loss(predr[mask], gt_left[mask])
-                    loss2 += CEloss(gt_left, self.maxdisp, gt_distribute, distributer)
+                    loss1 = loss1 + F.smooth_l1_loss(predr[mask], gt_left[mask])
+                    loss2 = loss2 + CEloss(gt_left, self.maxdisp, gt_distribute, distributer)
             else:
                 loss2 = loss1
 
